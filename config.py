e285495e8dff778b1534872885f19bcf29e7c06c
Mert <62549656+fswair@users.noreply.github.com>
2023-11-03 13:47:39 +0300

    la vita e bella

---
+++ config.py
@@ -0,0 +1,15 @@
+"""
+# You can get your API ID and API HASH from https://my.telegram.org
+# You can get your bot token from https://t.me/BotFather
+# You can get your rapid api key from https://rapidapi.com/Glavier/api/online-code-compiler
+"""
+
+NAME = "gccompiler"
+API_ID = None
+API_HASH =  ""
+BOT_TOKEN = ""
+RAPID_API_KEY = ""
+
+CPP17 = "17"
+CPP14 = "14"
+CPP11 = "11"
